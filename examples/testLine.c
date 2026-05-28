ca32828d107b0fbd5ed22e7211b6c5310a1a1d44
Marco Antonio Armendariz Paredes <marco.ap3101@gmail.com>
2026-05-28 01:32:03 -0500

    Actividad2: Implementación de color según la profundidad, también se movío el angulo de la tortuga para realizar el arbol en vertical

--- examples/testLine.c
+++ examples/testLine.c
@@ -1,18 +1,24 @@
 #include "../turtlec.h"
 
 void fractalTree(Turtle *turtle, float length, int depth){
-    if(depth == 0 || length < 5)
+  if(depth == 0 || length < 5)
         return;
-    turtleForward(turtle, length);
-
-    turtleLeft(turtle, 20);
-    fractalTree(turtle, length * 0.6, depth - 1);
     
-    turtleRight(turtle, 40);
-    fractalTree(turtle, length * 0.6, depth - 1);
+  turtleForward(turtle, length);
 
-    turtleLeft(turtle, 20);
-    turtleBackward(turtle, length);
+  turtleLeft(turtle, 20);
+  fractalTree(turtle, length * 0.6, depth - 1);
+    
+  turtleRight(turtle, 40);
+  fractalTree(turtle, length * 0.6, depth - 1);
+
+  if(depth > 5)
+    turtleSetColor(turtle, 120, 70, 20);
+  else
+    turtleSetColor(turtle, 0, 200, 0);
+
+  turtleLeft(turtle, 20);
+  turtleBackward(turtle, length);
 }
 
 int main(void){
@@ -25,12 +31,13 @@ int main(void){
 
   turtlePenUp(t);
   turtleGoTo(t,300.0f, 400.0f);
+  turtleLeft(t, 90);
   turtlePenDown(t);
 
   turtleSetColor(t, 255, 100, 0);
   turtleSetSpeed(t, 5.0f);
 
-  fractalTree(t, 100, 7);
+  fractalTree(t, 100, 8);
 
   turtleAppRun(app);
   turtleAppDestroy(app);
