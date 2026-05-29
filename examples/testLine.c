1d8479bf926a01436392ce5f88ff3edbac50692f
Marco Antonio Armendariz Paredes <marco.ap3101@gmail.com>
2026-05-29 02:52:02 -0500

    Retos: Implementación del reto 2 (fractalTreeR2) con longitud de la rama izquierda 0.4 de la longitud y la rama derecha 0.6 de la longitud

--- examples/testLine.c
+++ examples/testLine.c
@@ -22,6 +22,21 @@ void fractalTree(Turtle *turtle, float length, int depth){
   turtleBackward(turtle, length);
 }
 
+void levy(Turtle *turtle, int length, int depth){
+  if(depth == 0){
+    turtleForward(turtle, length);
+    return;
+  }
+
+  turtleRight(turtle, 45);
+  levy(turtle, length / sqrt(2), depth - 1);
+
+  turtleLeft(turtle, 90);
+  levy(turtle, length / sqrt(2), depth - 1);
+
+  turtleRight(turtle, 45);
+}
+
 void fractalTreeR1(Turtle *turtle, float length, int depth){
   if(depth == 0 || length < 5)
         return;
@@ -46,21 +61,21 @@ void fractalTreeR1(Turtle *turtle, float length, int depth){
   turtleBackward(turtle, length);
 }
 
-void levy(Turtle *turtle, int length, int depth){
-  if(depth == 0){
-    turtleForward(turtle, length);
-    return;
-  }
+void fractalTreeR2(Turtle *turtle, float length, int depth){
+  if(depth == 0 || length < 2)
+        return;
+    
+  turtleForward(turtle, length);
 
-  turtleRight(turtle, 45);
-  levy(turtle, length / sqrt(2), depth - 1);
+  turtleLeft(turtle, 35);
+  fractalTreeR2(turtle, length * 0.4, depth - 1);
+    
+  turtleRight(turtle, 70);
+  fractalTreeR2(turtle, length * 0.7, depth - 1); 
 
-  turtleLeft(turtle, 90);
-  levy(turtle, length / sqrt(2), depth - 1);
-
-  turtleRight(turtle, 45);
+  turtleLeft(turtle, 35);
+  turtleBackward(turtle, length);
 }
-
 
 int main(void){
   TurtleApp *app = turtleAppCreate(1200, 800, "Test Line");
@@ -71,18 +86,20 @@ int main(void){
   Turtle *t = turtleAppGetTurtle(app);
 
   turtlePenUp(t);
-  turtleGoTo(t,400.0f, 400.0f);
+  turtleGoTo(t,500.0f, 600.0f);
   turtleLeft(t, 90);
   turtlePenDown(t);
 
-  turtleSetColor(t, 255, 100, 0);
+  turtleSetColor(t, 30, 10, 210);
   turtleSetSpeed(t, 5.0f);
 
   //fractalTree(t, 100, 8);
 
   //levy(t, 200, 8);
 
-  fractalTreeR1(t, 150, 7);
+  //fractalTreeR1(t, 150, 7);
+
+  fractalTreeR2(t, 150, 5);
 
   turtleAppRun(app);
   turtleAppDestroy(app);
